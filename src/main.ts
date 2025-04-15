fd499a4beb1375cd26c67de63105c6077cef75f1
Arodrigow <rodrigow_@hotmail.com.br>
2025-04-15 14:41:12 -0300

    Added Exception Filter

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,15 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './utils/errors/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
   );
+
+  app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
