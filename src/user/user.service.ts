31d508796bde2bf3316743762f38d4f258440ef6
Arodrigow <rodrigow_@hotmail.com.br>
2025-04-16 15:07:55 -0300

    Added dependencies and files necessary to add auth

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,63 +6,75 @@ import * as bc from 'bcrypt';
 
 @Injectable()
 export class UserService {
-    constructor(private readonly prisma: PrismaService, private readonly configService: ConfigService) { }
+  constructor(private readonly prisma: PrismaService, private readonly configService: ConfigService) { }
 
-    async createUser(data: Prisma.UserCreateInput): Promise<User> {
-        const saltRounds = this.configService.get<string>('HASH_SALTROUNDS');
-        if (!saltRounds) {
-          throw new BadRequestException('Salt rounds not defined in config');
-        }
-      
-        const { password, ...rest } = data;
-        if (!password) {
-          throw new BadRequestException('Password is required');
-        }
-      
-        try {
-          const salt = await bc.genSalt(parseInt(saltRounds, 10));
-          const hashedPassword = await bc.hash(password, salt);
-      
-          const user = await this.prisma.user.create({
-            data: {
-              ...rest,
-              password: hashedPassword,
-            },
-          });
-      
-          return user;
-        } catch (err) {
-          throw new BadRequestException('Error creating user: ' + err.message);
-        }
-      }
-
-    async getAllUser() {
-        return this.prisma.user.findMany();
+  async createUser(data: Prisma.UserCreateInput): Promise<User> {
+    const saltRounds = this.configService.get<string>('HASH_SALTROUNDS');
+    if (!saltRounds) {
+      throw new BadRequestException('Salt rounds not defined in config');
     }
 
-    async getUserById(id: number): Promise<User> {
-        const user = await this.prisma.user.findUnique({
-            where: { id },
-        });
-        // Check if user exists 
-        if (!user) {
-            throw new NotFoundException('User not found');
-        }
-
-        return user
+    const { password, ...rest } = data;
+    if (!password) {
+      throw new BadRequestException('Password is required');
     }
 
-    async updateUser(id: number, data: Prisma.UserUpdateInput): Promise<User> {
-        return await this.prisma.user.update({
-            where: { id },
-            data,
-        });
+    try {
+      const salt = await bc.genSalt(parseInt(saltRounds, 10));
+      const hashedPassword = await bc.hash(password, salt);
+
+      const user = await this.prisma.user.create({
+        data: {
+          ...rest,
+          password: hashedPassword,
+        },
+      });
+
+      return user;
+    } catch (err) {
+      throw new BadRequestException('Error creating user: ' + err.message);
+    }
+  }
+
+  async getAllUser() {
+    return this.prisma.user.findMany();
+  }
+
+  async getUserById(id: number): Promise<User> {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+    });
+    // Check if user exists 
+    if (!user) {
+      throw new NotFoundException('User not found');
     }
 
-    async deleteUser(id: number): Promise<User> {
-        return await this.prisma.user.delete({
-            where: { id },
-        });
+    return user
+  }
+
+  async getUserByEmail(email: string): Promise<User> {
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
+    // Check if user exists 
+    if (!user) {
+      throw new NotFoundException('User not found');
     }
+
+    return user
+  }
+
+  async updateUser(id: number, data: Prisma.UserUpdateInput): Promise<User> {
+    return await this.prisma.user.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async deleteUser(id: number): Promise<User> {
+    return await this.prisma.user.delete({
+      where: { id },
+    });
+  }
 
 }
