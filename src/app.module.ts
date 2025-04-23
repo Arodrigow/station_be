7f5d7fda0b1e96f3adf11df3d5c3d414b5567bc8
Arodrigow <rodrigow_@hotmail.com.br>
2025-04-23 09:59:30 -0300

    Added station reading files

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { StationModule } from './station/station.module';
+import { ReadingModule } from './reading/reading.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, UserModule, AuthModule, StationModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, UserModule, AuthModule, StationModule, ReadingModule],
   controllers: [AppController],
   providers: [AppService]
 })
