7508df3258792f4936b02d86d2f000f5a17dd54d
Arodrigow <rodrigow_@hotmail.com.br>
2025-04-24 10:25:13 -0300

    Added restriction to creating an user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,7 +9,8 @@ import { Roles } from 'src/auth/roles/roles.decorator';
 export class UserController {
     constructor(private readonly userService: UserService) { }
 
-    //TODO: Only admin can create user, do it before production
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.ADMIN) 
     @Post()
     createUser(@Body() body: Prisma.UserCreateInput) {
         return this.userService.createUser(body);
