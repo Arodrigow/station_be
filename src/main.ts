a04207ce9395e1c2127144481758b725bb96dade
Arodrigow <rodrigow_@hotmail.com.br>
2025-04-24 11:26:07 -0300

    Fixed address

--- src/main.ts
+++ src/main.ts
@@ -32,6 +32,6 @@ async function bootstrap() {
   });
   
   app.useGlobalFilters(new AllExceptionsFilter());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
